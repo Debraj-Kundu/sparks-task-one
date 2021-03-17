c878f9015978bf786d552ab11a37f5eb645ac21d
Debraj Kundu <debrajk27@gmail.com>
2021-03-17 10:27:01 +0530

    deploy

--- server.js
+++ server.js
@@ -17,13 +17,13 @@ app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
 
-/* app.get('/', async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     res.render('employee/home');
   } catch (err) {
     console.log('error');
   }
-}); */
+});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log('Express server started!');
