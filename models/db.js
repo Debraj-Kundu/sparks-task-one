b5fdf67e248627e2ab508bce674da3a07a09e165
Debraj Kundu <debrajk27@gmail.com>
2021-03-17 10:05:32 +0530

    deploy

--- models/db.js
+++ models/db.js
@@ -2,10 +2,11 @@ const mongoose = require('mongoose');
 
 const str = 'mongodb://localhost:27017/EmployeeDB1';
 
+const uri = process.env.ATLAS_URI || str;
 //mongoose.createConnection(str, { useNewUrlParser: true, useUnifiedTopology: true });
 
 async function callDB() {
-  await mongoose.connect(str, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err) => {
+  await mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, (err) => {
     if (!err) { console.log('MongoDB connection success'); }
     else { console.log('MongoDB connection error: ' + err); }
   });
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const path = require('path');
 const exphbs = require('express-handlebars');
 const employeeController = require('./controllers/employeeController');
 const bodyParser = require('body-parser');
+require('dotenv').config();
 
 
 const app = express();
@@ -15,6 +16,14 @@ app.use(bodyParser.json());
 app.set('views', path.join(__dirname, '/views/'));
 app.engine('hbs', exphbs({ extname: 'hbs', defaultLayout: 'mainLayout', layoutsDir: __dirname + '/views/layouts/' }));
 app.set('view engine', 'hbs');
+
+/* app.get('/', async (req, res) => {
+  try {
+    res.render('employee/home');
+  } catch (err) {
+    console.log('error');
+  }
+}); */
 
 app.listen(3000, () => {
   console.log('Express server started!');
